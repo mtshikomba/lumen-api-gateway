a558ec11be7cba5836a2793d08bbac5678df93be
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-24 21:39:37 -0500

    api responses added

---
+++ app/Http/Traits/ApiResponser.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponsor{
+
+    /**
+     * Build a success response
+     * @param string|array $data
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function successReponse($data, $code = Response::HTTP_OK){
+        return response($data, $code)->header('Content-Type', 'application/json');
+    }
+
+
+    /**
+     * Build a success response
+     * @param string|array $message
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    /**
+     * Build a success response
+     * @param string|array $message
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorMessage($message, $code)
+    {
+        return response($message, $code)->header('Content-Type', 'application/json');
+    }
+}
+
+?>
