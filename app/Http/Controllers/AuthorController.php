57e507d0bdb62fd6ad0a7ea56b2788459cf1a77f
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-24 21:40:07 -0500

    controllers added

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    public function destroy($author)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\book;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+        
+    }
+
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+    /**
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+
+    }
+
+    public function destroy($book)
+    {
+
+    }
+}
