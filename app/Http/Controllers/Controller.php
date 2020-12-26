8c8cb4b241404a65234bf570a1b83b7af177f44c
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-26 14:02:50 -0500

    api responser added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponsor;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    use ApiResponsor;
 }
