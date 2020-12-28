088e6085a9ed498e9d82063977e77bcdc781d0e6
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-28 16:45:40 -0500

    Gateway Exception handling configured successfully

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponsor;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -11,6 +15,7 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponsor;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +54,45 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse($message, $code);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $model = class_basename($exception->getModel());
+
+            return $this->errorResponse("{$model} with given id could not be found", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($exception instanceof ClientException) {
+            $message = $exception->getResponse()->getBody();
+            $code = $exception->getCode();
+
+            return $this->errorMessage($message, $code);
+        }
+
+        // check if we are in production or not
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected error, please try again later', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
